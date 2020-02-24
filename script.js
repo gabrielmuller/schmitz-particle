9f8b613e4546d60c694c7e723c328611f36b45b9
Gabriel Müller <gabrielmullerw@gmail.com>
2020-02-24 20:58:48 -0300

    Corners

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let ctx = document.getElementById("demo").getContext('2d');
 const S = document.body.offsetWidth * 0.7; // S is shorthand for size
 ctx.canvas.width  = S;
 ctx.canvas.height  = S;
+Object.getPrototypeOf(ctx).circle = (x, y, r) => ctx.arc(x, y, r, 0, Math.PI * 2);
 
 let cellSize = S * 0.8;
 let cellStart = (S - cellSize) / 2;
@@ -14,7 +15,7 @@ window.requestAnimationFrame(draw);
 function draw() {
     ctx.clearRect(0, 0, S, S);
     ctx.strokeStyle = '#222';
-    ctx.lineWidth = 5;
+    ctx.lineWidth = Math.round(S * 0.01);
     ctx.beginPath();
 
     ctx.moveTo(0, cellStart);
@@ -30,6 +31,18 @@ function draw() {
     ctx.lineTo(cellEnd, S);
 
     ctx.stroke()
+
+    ctx.fillStyle = 'white';
+
+    [cellStart, cellEnd].forEach((x, _) => {
+        [cellStart, cellEnd].forEach((y, _) => {
+            ctx.beginPath();
+            ctx.circle(x, y, 0.04 * S);
+            ctx.fill();
+            ctx.stroke()
+        });
+    });
+
     pos++;
-    window.requestAnimationFrame(draw);
+    //window.requestAnimationFrame(draw);
 }
