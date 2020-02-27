6c5797b918dc3592ff377903b17633d192f12712
gabrielmuller <gabrielmullerw@gmail.com>
2020-02-27 00:12:09 -0300

    Approximation done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const S = document.body.offsetWidth * 1; // S is shorthand for size
 function circle(ctx, x, y, r) {
     ctx.arc(x, y, r, 0, Math.PI * 2); 
 }
-const headLength = S * 0.02;
+const headLength = S * 0.014;
 const headAngle = 0.6;
 function arrow(ctx, ox, oy, dx, dy)  {
     let tx = ox + dx;
@@ -35,7 +35,7 @@ function normalPlane(ctx, ox, oy, nx, ny) {
     ctx.strokeStyle = 'darkblue';
     arrow(ctx, ox, oy, nx * normalLength, ny * normalLength);
     ctx.strokeStyle = '#2222FF90';
-    ctx.lineWidth = 3;
+    ctx.fillStyle = '#2222FF40';
     ctx.beginPath();
     const px = ny;
     const py = -nx;
@@ -97,6 +97,7 @@ let input = [
     new Hermite(cellEnd, cellEnd, 0, -1),
 ];
 
+const strength = 0.3;
 function draw() {
     ctx.clearRect(0, 0, S, S);
     ctx.strokeStyle = '#444';
@@ -119,26 +120,18 @@ function draw() {
 
     ctx.fillStyle = 'white';
 
-    [cellStart, cellEnd].forEach((x, _) => {
-        [cellStart, cellEnd].forEach((y, _) => {
-            ctx.beginPath();
-            circle(ctx, x, y, 0.04 * S);
-            ctx.fill();
-            ctx.stroke()
-        });
-    });
-
     ctx.lineWidth = S * 0.006;
     input[0].setNormal(Math.sin(p), Math.cos(p));
     input[1].setNormal(Math.sin(p * 1.618), Math.cos(p * 1.618));
-    input[2].setNormal(0.3, 0.7);
-    input[3].setNormal(-0.3, -0.7);
+    input[2].setNormal(Math.sin(p * 1.618 * 0.1), Math.cos(p * 1.618 * 0.8));
+    input[3].setNormal(Math.sin(p * 1.618 * 0.01), Math.cos(p * 1.618) * 0.02);
     input[0].t = Math.abs(Math.sin(p));
     input[1].t = Math.abs(Math.cos(p));
-    input[3].t = 0.7;
-    input[2].t = 0.7;
+    input[3].t = Math.abs(Math.sin(p * 0.1518));
+    input[2].t = Math.abs(Math.cos(p * 0.4683));
     let centerX = 0;
     let centerY = 0;
+    ctx.lineWidth = 3;
     input.forEach((hermite, _) => {
         hermite.update();
         console.log(hermite);
@@ -146,34 +139,61 @@ function draw() {
         centerX += hermite.posX;
         centerY += hermite.posY;
     });
-    ctx.strokeStyle = 'darkgreen';
-
-    [cellStart, cellEnd].forEach((x, _) => {
-        [cellStart, cellEnd].forEach((y, _) => {
-            sumX = 0;
-            sumY = 0;
-            input.forEach((hermite, _) => {
-                ctx.lineWidth = 3;
-                const [vx, vy] = hermite.vectorTo(x, y);
-                arrow(ctx, x, y, -vx, -vy);
-                sumX -= vx;
-                sumY -= vy;
-            });
-            ctx.lineWidth = 6;
-            arrow(ctx, x, y, sumX, sumY);
-        });
-    });
 
     centerX /= input.length;
     centerY /= input.length;
 
+    let partialForcesX = [0, 0, 0, 0];
+    let partialForcesY = [0, 0, 0, 0];
+
+    ctx.strokeStyle = 'darkgreen';
+
+    [cellStart, cellEnd].forEach((x, i) => {
+        [cellStart, cellEnd].forEach((y, j) => {
+            let k = i * 2 + j;
+            input.forEach((hermite, _) => {
+                const [vx, vy] = hermite.vectorTo(x, y);
+                //arrow(ctx, x, y, -vx, -vy);
+                partialForcesX[k] -= vx;
+                partialForcesY[k] -= vy;
+            });
+            ctx.lineWidth = 6;
+            //arrow(ctx, x, y, partialForcesX[k], partialForcesY[k]);
+        });
+    });
+
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'red';
     ctx.fillStyle = 'red';
-    ctx.strokeStyle = '#222';
-    ctx.beginPath();
-    circle(ctx, centerX, centerY, S * 0.01);
-    ctx.fill();
+    for (let i = 0; i < 32; i++) {
+
+        ctx.beginPath();
+        circle(ctx, centerX, centerY, S * 0.004);
+        ctx.fill();
+
+
+        forceX = 0;
+        forceY = 0;
+        [cellStart, cellEnd].forEach((x, i) => {
+            [cellStart, cellEnd].forEach((y, j) => {
+                let k = i * 2 + j;
+                let area = (1-Math.abs(centerX-x)/cellSize) * (1-Math.abs(centerY-y)/cellSize)
+                forceX += partialForcesX[k] * area;
+                forceY += partialForcesY[k] * area;
+            });
+        });
+
+        forceX *= strength;
+        forceY *= strength;
+        arrow(ctx, centerX, centerY, forceX, forceY);
+
+        centerX += forceX;
+        centerY += forceY;
+        centerX = Math.min(Math.max(centerX, cellStart), cellEnd);
+        centerY = Math.min(Math.max(centerY, cellStart), cellEnd);
+    }
 
     
-    p += 0.01;
+    p += 0.005;
     window.requestAnimationFrame(draw);
-}
+    }
